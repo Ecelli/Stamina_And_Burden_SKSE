8d1c44a6f8157f6c425a13506d3c65198ddcb42f
Ecelli <Ecelli@nexus.com>
2026-06-20 16:58:19 -0400

    Reworked sprint stamina formula
    This will make it better aligned with our other stamina regeneration
    formulas based on burden (same principles).
    
    Core idea:
    Action cost: stamina cost based on burden % + maxStamina% cost based on
    carry burden.
    
    This make it so no matter how high stamina can get, the player would be
    unable to sprint "forever"

--- src/Regen/CostsManager.cpp
+++ src/Regen/CostsManager.cpp
@@ -13,13 +13,23 @@ namespace Costs
 		auto& burden = Burden::Tracker::GetOrComputeBurden(actor);
 		auto* params = CostsParams::GetSingleton();
 
-		float base = params->SprintDrainBase.Get();
-		float burdenPenalty = params->SprintDrainBurdenPenalty.Get() * burden.burden;
-		float drainPerSecond = base + burdenPenalty;
-		float result = drainPerSecond * RE::GetSecondsSinceLastFrame();
+        float Stamina_1pctMax = 0.01f * actor->GetActorValueMax(RE::ActorValue::kStamina);
+        float SprintBurdenFlat = Math::Interpolate(
+			params->SprintDrainLowBurden.Get(),
+			params->SprintDrainHighBurden.Get(),
+			burden.burden,
+			params->SprintDrainBurdenCurve_k.Get());
+        float SprintBurdenMult = Math::Interpolate(
+			params->SprintDrainLowCarryBurdenPct.Get(),
+			params->SprintDrainHighCarryBurdenPct.Get(),
+			burden.carryBurden,
+			params->SprintDrainCarryBurdenCurve_k.Get());
 
-		Regen::RegenLog("CalculateSprintDrain: base={:.3f} burdenPen={:.3f} drain/s={:.3f} delta={:.6f} -> {:.3f}",
-			base, burdenPenalty, drainPerSecond, RE::GetSecondsSinceLastFrame(), result);
+        float TotalCost = SprintBurdenFlat + SprintBurdenMult* Stamina_1pctMax;
+		float result = TotalCost * RE::GetSecondsSinceLastFrame();
+
+		Regen::RegenLog("CalculateSprintDrain: BurdenCost={:.3f} BurdenStamina pct={:.3f} drain/s={:.3f} delta={:.6f} -> {:.3f}",
+			SprintBurdenFlat, SprintBurdenMult, TotalCost, RE::GetSecondsSinceLastFrame(), result);
 		return result;
 	}
 }
--- src/Settings/Params/CostsParams.h
+++ src/Settings/Params/CostsParams.h
@@ -6,15 +6,24 @@ namespace Costs
 {
 	struct CostsParams : REX::Singleton<CostsParams>
 	{
-		Parameter<float> SprintDrainBase{ 3.0f, 0.0f, 200.0f };
-		Parameter<float> SprintDrainBurdenPenalty{ 12.0f, 0.0f, 200.0f };
+        Parameter<float> SprintDrainLowBurden{ 3.0f, 0.0f, 200.0f };
+		Parameter<float> SprintDrainHighBurden{ 15.0f, 0.0f, 200.0f };
+		Parameter<float> SprintDrainLowCarryBurdenPct{ 0.1f, 0.0f, 60.0f };
+		Parameter<float> SprintDrainHighCarryBurdenPct{ 10.0f, 0.0f, 60.0f };
+		Parameter<float> SprintDrainBurdenCurve_k{ 0.8f, 0.0f, 1.0f };
+		Parameter<float> SprintDrainCarryBurdenCurve_k{ 0.9f, 0.0f, 1.0f };
+	
 
 		template <typename F>
 		static void ForEach(F&& a_fn)
 		{
 			auto& s = GetSingleton();
-			a_fn("fSprintDrainBase"sv, s.SprintDrainBase);
-			a_fn("fSprintDrainBurdenPenalty"sv, s.SprintDrainBurdenPenalty);
+			a_fn("fSprintDrainLowBurden"sv, s.SprintDrainLowBurden);
+			a_fn("fSprintDrainHighBurden"sv, s.SprintDrainHighBurden);
+			a_fn("fSprintDrainLowCarryBurdenPct"sv, s.SprintDrainLowCarryBurdenPct);
+			a_fn("fSprintDrainHighCarryBurdenPct"sv, s.SprintDrainHighCarryBurdenPct);
+			a_fn("fSprintDrainBurdenCurve_k"sv, s.SprintDrainBurdenCurve_k);
+			a_fn("fSprintDrainCarryBurdenCurve_k"sv, s.SprintDrainCarryBurdenCurve_k);
 		}
 	};
 
