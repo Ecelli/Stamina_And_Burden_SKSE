2d6d0c33a63bfb69b1b7bbe3bb9ba16ee7eec2bd
Ecelli <Ecelli@nexus.com>
2026-06-29 22:00:25 -0400

    Moved damage log to utils where it should live

--- src/Common/Utils.h
+++ src/Common/Utils.h
@@ -5,6 +5,7 @@
 #include <thread>
 #include "Settings/Params/RegenParams.h"
 #include "Settings/Params/CostsParams.h"
+#include "Settings/Params/DamageParams.h"
 
 namespace Common
 {
@@ -45,6 +46,17 @@ namespace Costs
     }
 }
 
+
+namespace Damage
+{
+    template <typename... Args>
+    void DamageLog(std::string_view a_fmt, Args&&... a_args)
+    {
+        if (DamageParams::GetSingleton()->EnableDebugLogging.Get()) {
+            logger::info(fmt::runtime(a_fmt), std::forward<Args>(a_args)...);
+        }
+    }
+}
 
 namespace Math
 {
--- src/Damage/DamageManager.h
+++ src/Damage/DamageManager.h
@@ -5,12 +5,4 @@
 namespace Damage
 {
 	float ComputeStaminaDamageMult(RE::Actor* actor);
-
-	template <typename... Args>
-	void DamageLog(std::string_view a_fmt, Args&&... a_args)
-	{
-		if (DamageParams::GetSingleton()->EnableDebugLogging.Get()) {
-			logger::info(fmt::runtime(a_fmt), std::forward<Args>(a_args)...);
-		}
-	}
 }
