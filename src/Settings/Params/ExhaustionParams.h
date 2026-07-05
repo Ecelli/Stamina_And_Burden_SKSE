2574f04bb0de1a62484de9192fcce9109afdbf2d
Ecelli <Ecelli@nexus.com>
2026-07-04 20:53:13 -0400

    Added exhaustion parameters.
    Inpired by both vulture dynamic realism and valhalla combat.
    IN vulture, it adds carryweight which is a very bad fir for this.
    We rely on a more similar to valhalla scheme.
    
    - If the actor has stamina 0 -> trigger exhaustion
    - If the actor stamina exceeds 50% (configurable) immediately terminate exhaustion
    - As long as stamina is >0 for 5 seconds (configurable), terminate exhaustion and recover 30% (configurable) stamina
    
    This provides a push pull mechanics. Should the player try to recover
    stamina quickly to exit exhaustion, play around and hold to gain the
    burst, push and finish the fight quickly?

---
+++ src/Settings/Params/ExhaustionParams.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Settings/Params/Parameter.h"
+
+struct ExhaustionParams : REX::Singleton<ExhaustionParams>
+{
+	// Master toggles
+	Parameter<bool>  bExhaustionPlayer{ true, false, true };
+	Parameter<bool>  bExhaustionNPC{ false, false, true };
+
+	// Recovery timing
+	Parameter<float> fExhaustionDuration{ 5.0f, 1.0f, 30.0f };
+	Parameter<float> fExhaustionBurstStamina{ 0.30f, 0.0f, 1.0f };
+	Parameter<float> fExhaustionThresholdStamina{ 0.50f, 0.0f, 1.0f };
+
+	// Regen and damage penalties
+	Parameter<float> fExhaustionPenaltyDamageMult{ 0.50f, 0.0f, 1.0f };
+	Parameter<float> fExhaustionPenaltyStaminaMult{ 0.30f, 0.0f, 1.0f };
+	Parameter<float> fExhaustionPenaltyHealthMult{ 0.0f, 0.0f, 1.0f };
+	Parameter<float> fExhaustionPenaltyMagickaMult{ 0.0f, 0.0f, 1.0f };
+
+	// Debug
+	Parameter<bool>  bEnableDebugLogging{ false, false, true };
+
+	template <typename F>
+	static void ForEach(F&& a_fn)
+	{
+		auto& s = GetSingleton();
+		a_fn("bExhaustionPlayer"sv, s.bExhaustionPlayer);
+		a_fn("bExhaustionNPC"sv, s.bExhaustionNPC);
+		a_fn("fExhaustionDuration"sv, s.fExhaustionDuration);
+		a_fn("fExhaustionBurstStamina"sv, s.fExhaustionBurstStamina);
+		a_fn("fExhaustionThresholdStamina"sv, s.fExhaustionThresholdStamina);
+		a_fn("fExhaustionPenaltyDamageMult"sv, s.fExhaustionPenaltyDamageMult);
+		a_fn("fExhaustionPenaltyStaminaMult"sv, s.fExhaustionPenaltyStaminaMult);
+		a_fn("fExhaustionPenaltyHealthMult"sv, s.fExhaustionPenaltyHealthMult);
+		a_fn("fExhaustionPenaltyMagickaMult"sv, s.fExhaustionPenaltyMagickaMult);
+		a_fn("bEnableDebugLogging"sv, s.bEnableDebugLogging);
+	}
+};
---
+++ src/Stamina/ExhaustionManager.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <unordered_map>
+
+struct ExhaustionState
+{
+	bool isExhausted = false;
+	float safeTimer = 0.0f;
+};
+
+class ExhaustionManager : public REX::Singleton<ExhaustionManager>
+{
+public:
+	void TriggerExhaustion(RE::Actor* a_actor);
+	bool IsExhausted(RE::Actor* a_actor);
+	void Update();
+	void ClearAll();
+
+private:
+	std::unordered_map<RE::FormID, ExhaustionState> states;
+
+	void ClearExhaustion(RE::FormID a_formId);
+};
