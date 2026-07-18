b5dca3f1823431d494c2f7c24c88595280b45a92
Ecelli <Ecelli@nexus.com>
2026-07-18 17:38:44 -0400

    Renamed papyrus to StaminaAndBurden. Added burden query APIs
    Only query important burden, carry burden, blended burden, max equipped
    weight and equippedweight (burden based)

---
+++ Data/Source/Scripts/StaminaAndBurden.psc
@@ -0,0 +1,10 @@
+Scriptname StaminaAndBurden
+
+Int[] Function GetVersion() Global Native
+
+; Burden queries
+Float Function GetBurden(Actor a_actor) Global Native
+Float Function GetCarryBurden(Actor a_actor) Global Native
+Float Function GetBurdenBlend(Actor a_actor) Global Native
+Float Function GetEquippedWeight(Actor a_actor) Global Native
+Float Function GetMaxEquippedWeight(Actor a_actor) Global Native
--- src/Papyrus/Papyrus.cpp
+++ src/Papyrus/Papyrus.cpp
@@ -1,4 +1,5 @@
 #include "papyrus.h"
+#include "Burden/BurdenTracker.h"
 
 namespace Papyrus
 {
@@ -6,9 +7,40 @@ namespace Papyrus
 		return { Plugin::VERSION[0], Plugin::VERSION[1], Plugin::VERSION[2] };
 	}
 
+	float GetBurden(STATIC_ARGS, RE::Actor* a_actor) {
+		if (!a_actor) return 0.0f;
+		return Burden::Tracker::GetOrComputeBurden(a_actor).burden;
+	}
+
+	float GetCarryBurden(STATIC_ARGS, RE::Actor* a_actor) {
+		if (!a_actor) return 0.0f;
+		return Burden::Tracker::GetOrComputeBurden(a_actor).carryBurden;
+	}
+
+	float GetBurdenBlend(STATIC_ARGS, RE::Actor* a_actor) {
+		if (!a_actor) return 0.0f;
+		return Burden::Tracker::GetOrComputeBurden(a_actor).burdenBlend;
+	}
+
+	float GetEquippedWeight(STATIC_ARGS, RE::Actor* a_actor) {
+		if (!a_actor) return 0.0f;
+		return Burden::Tracker::GetOrComputeBurden(a_actor).equippedWeight;
+	}
+
+	float GetMaxEquippedWeight(STATIC_ARGS, RE::Actor* a_actor) {
+		if (!a_actor) return 0.0f;
+		return Burden::Tracker::GetOrComputeBurden(a_actor).maxEquippedWeight;
+	}
+
 	void Bind(VM& a_vm) {
 		logger::info("  >Binding GetVersion..."sv);
 		BIND(GetVersion);
+		logger::info("  >Binding burden queries..."sv);
+		BIND(GetBurden);
+		BIND(GetCarryBurden);
+		BIND(GetBurdenBlend);
+		BIND(GetEquippedWeight);
+		BIND(GetMaxEquippedWeight);
 	}
 
 	bool RegisterFunctions(VM* a_vm) {
