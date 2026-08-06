770e718baadfd272dd4be0c1ad22065a01c12a29
Ecelli <Ecelli@nexus.com>
2026-08-05 21:52:19 -0400

    Added LOCKMAP class for mutex hardening of the mod
    I've found hard to reproduce crashes with NPCs, seem to be related to
    potential race conditions.
    
    I'm fixing this issue by:
    1- Adding a lock map so all unordered map operations in the mod are
    locked by mutex
    2- Map read values only by values and not by pointers. It prevents a
    potential async clear from garbling the value

---
+++ src/Common/LockedMap.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstddef>
+#include <shared_mutex>
+#include <unordered_map>
+
+template <class Key, class Value, class Hash = std::hash<Key>>
+class LockedMap
+{
+public:
+	using size_type = std::size_t;
+
+	LockedMap() = default;
+	LockedMap(const LockedMap&) = delete;
+	LockedMap& operator=(const LockedMap&) = delete;
+
+	void insert_or_assign(const Key& a_key, Value a_value)
+	{
+		std::unique_lock lock(m_mtx);
+		// equivalent to m_map[a_key] = a_value
+		m_map.insert_or_assign(a_key, std::move(a_value));
+	}
+
+	size_type erase(const Key& a_key)
+	{
+		std::unique_lock lock(m_mtx);
+		return m_map.erase(a_key);
+	}
+
+	void clear()
+	{
+		std::unique_lock lock(m_mtx);
+		m_map.clear();
+	}
+
+	bool get(const Key& a_key, Value& a_out) const
+	{
+        // Returns by value
+		std::shared_lock lock(m_mtx);
+		auto it = m_map.find(a_key);
+		if (it == m_map.end())
+			return false;
+		a_out = it->second;
+		return true;
+	}
+
+private:
+	mutable std::shared_mutex m_mtx;
+	std::unordered_map<Key, Value, Hash> m_map;
+};
